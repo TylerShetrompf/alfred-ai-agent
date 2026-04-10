804873d66b4873c38d4578412b07f7d29579e83b
Tyler Shetrompf <tyler@shetrompf.com>
2026-04-10 14:51:08 -0400

    Migrated code out into functions for organization purposes

--- main.py
+++ main.py
@@ -1,16 +1,37 @@
 import os
-from xmlrpc import client # for accessing environment variables
 from dotenv import load_dotenv # for reading environment variables from .env file
 from google import genai # for interacting with Gemini API
 import argparse # for parsing arguments passed via cli
+from google.genai import types 
 
-def main():
+# function to call api with content
+def call_model(client, messages):
+    
+    # send prompt and return response
+    response = client.models.generate_content( 
+        model = "gemini-3-flash-preview", 
+        contents = messages
+    )
 
-    # Parse prompt given via cli
-    prompt_parser = argparse.ArgumentParser(description="Alfred")
-    prompt_parser.add_argument("user_prompt", type = str, help = "Prompt for Alfred")
-    args = prompt_parser.parse_args()
+    return response
 
+# function to check for response metadata, then print token info and return text. otherwise raise error
+def check_metadata(response):
+    
+    if response.usage_metadata != None:
+        
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+        
+        return response.text
+    
+    else:
+        
+        raise RuntimeError("No usage_metadata received, API request failure likely.")
+
+
+def get_api_key():
+    
     #loads .env file and gets api key
     load_dotenv()
     api_key = os.getenv("GEMINI_API_KEY")
@@ -19,23 +40,34 @@ def main():
     if api_key == None:
         raise RuntimeError("GEMINI_API_KEY environment variable not set.")
 
+    return api_key
+
+def main():
+
+    # Parse prompt given via cli
+    prompt_parser = argparse.ArgumentParser(description="Alfred")
+    prompt_parser.add_argument("user_prompt", type = str, help = "Prompt for Alfred")
+    args = prompt_parser.parse_args()
+
+    # call func to retrieve api key
+    api_key = get_api_key()
+
     # create client object with api_key
     client = genai.Client(api_key=api_key)
 
-    # send prompt and store response
-    response = client.models.generate_content( 
-        model = "gemini-3-flash-preview", 
-        contents = args.user_prompt
-    )
+    # set users prompt as message
+    messages = [types.Content(role="user", parts=[types.Part(text = args.user_prompt)])]
 
-    # check for response metadata, then print info. otherwise raise error
-    if response.usage_metadata != None:
-        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
-        print(response.text)
-    else:
-        raise RuntimeError("No usage_metadata received, API request failure likely.")
+    # call model and store entire response
+    response = call_model(client, messages)
+    
+    # send response to metadata check function
+    response_text = check_metadata(response)
 
+    # print response to CLI
+    print(response_text)
+
+    
 
 if __name__ == "__main__":
     main()
