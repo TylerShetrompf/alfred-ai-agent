d5d899ccb3afcc445399c55db146f3386cdc9e0d
Tyler Shetrompf <tyler@shetrompf.com>
2026-04-10 14:00:43 -0400

    Added cli argument for prompt, error handling, and output for tracking token consumption

--- main.py
+++ main.py
@@ -2,22 +2,39 @@ import os
 from xmlrpc import client # for accessing environment variables
 from dotenv import load_dotenv # for reading environment variables from .env file
 from google import genai # for interacting with Gemini API
+import argparse # for parsing arguments passed via cli
 
 def main():
+
+    # Parse prompt given via cli
+    prompt_parser = argparse.ArgumentParser(description="Alfred")
+    prompt_parser.add_argument("user_prompt", type = str, help = "Prompt for Alfred")
+    args = prompt_parser.parse_args()
+
+    #loads .env file and gets api key
     load_dotenv()
     api_key = os.getenv("GEMINI_API_KEY")
 
+    # raise an error if api_key not found
     if api_key == None:
         raise RuntimeError("GEMINI_API_KEY environment variable not set.")
 
+    # create client object with api_key
     client = genai.Client(api_key=api_key)
 
-    response = client.models.generate_content( # test call
+    # send prompt and store response
+    response = client.models.generate_content( 
         model = "gemini-3-flash-preview", 
-        contents = "Please confirm that you have received this message. Reply in only one sentence, utilizing as few tokens as possible."
+        contents = args.user_prompt
     )
-    
-    print(response.text)
+
+    # check for response metadata, then print info. otherwise raise error
+    if response.usage_metadata != None:
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+        print(response.text)
+    else:
+        raise RuntimeError("No usage_metadata received, API request failure likely.")
 
 
 if __name__ == "__main__":
