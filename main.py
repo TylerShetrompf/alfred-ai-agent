1d33cc8bde492a05e01ea89ab6b34e0fbb49de24
Tyler Shetrompf <tyler@shetrompf.com>
2026-04-10 01:56:49 -0400

    Initial tests, error handling.

--- main.py
+++ main.py
@@ -1,5 +1,23 @@
+import os
+from xmlrpc import client # for accessing environment variables
+from dotenv import load_dotenv # for reading environment variables from .env file
+from google import genai # for interacting with Gemini API
+
 def main():
-    print("Hello from alfred-ai-agent!")
+    load_dotenv()
+    api_key = os.getenv("GEMINI_API_KEY")
+
+    if api_key == None:
+        raise RuntimeError("GEMINI_API_KEY environment variable not set.")
+
+    client = genai.Client(api_key=api_key)
+
+    response = client.models.generate_content( # test call
+        model = "gemini-3-flash-preview", 
+        contents = "Please confirm that you have received this message. Reply in only one sentence, utilizing as few tokens as possible."
+    )
+    
+    print(response.text)
 
 
 if __name__ == "__main__":
